5970ac6611723dc019e1e56898067f5894b5d07d
Nolan Conaway <nconaway@shutterstock.com>
2018-08-23 12:10:45 -0400

    working

---
+++ spec/reveal-in-finder-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import RevealInFinderView from '../lib/reveal-in-finder-view';
+
+describe('RevealInFinderView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
